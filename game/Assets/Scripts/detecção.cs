4ab2d91545957b63f8bf2ce9c50c863f3c6a23e9
samuks123 <samubernacci@gmail.com>
2022-11-02 17:17:47 -0300

    detecção por tempo(precisa de um indicador)

--- game/Assets/Scripts/detecção.cs
+++ game/Assets/Scripts/detecção.cs
@@ -5,7 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class detecção : MonoBehaviour
 {
+    Coroutine co;
+    
     private void OnTriggerEnter2D(Collider2D other){
+        co = StartCoroutine(DetectionTime());
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        StopCoroutine(co);
+    }
+
+    IEnumerator DetectionTime(){
+        yield return new WaitForSecondsRealtime(1);
+
         PlayerPrefs.SetInt("player_money", 0);
         SceneManager.LoadScene("Saida", LoadSceneMode.Single);
     }
