a461d00f8ffa281cb8818d4464adcfad4290aba2
Vinícius Santos Monteiro <vini.mon@usp.br>
2022-11-10 00:13:43 -0300

    novo jorjinho, animacao andar (front), animacao idle

--- game/Assets/Scripts/movimentation.cs
+++ game/Assets/Scripts/movimentation.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class movimentation : MonoBehaviour
 {
+    
+    public Animator animator;
+
     int speed = 10;
+
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
 
     void Update()
     {
+
+        animator.SetBool("isWalkingFront", false);
 
         if( Input.GetButton("Horizontal") ){
             transform.Translate( Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
@@ -15,6 +27,9 @@ public class movimentation : MonoBehaviour
 
         if( Input.GetButton("Vertical") ){
             transform.Translate( 0, Input.GetAxis("Vertical") * Time.deltaTime * speed, 0);
+            if( Input.GetAxis("Vertical") < 0 ){
+                animator.SetBool("isWalkingFront", true);
+            }
         }
 
     }
