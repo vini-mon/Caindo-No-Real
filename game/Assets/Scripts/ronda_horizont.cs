3bd28f45c8e199d04d15bc90cc5ecc3b23a171f3
Vinícius Santos Monteiro <vini.mon@usp.br>
2022-11-21 18:36:33 -0300

    varias mudanças radicais

--- game/Assets/Scripts/ronda_horizont.cs
+++ game/Assets/Scripts/ronda_horizont.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class ronda_horizont : MonoBehaviour
 {
+
+    public Animator animator;
+
     int speed = 3;
     float tempo = 0.0f, tempo2 = 0.0f;
 
+    void Start(){
+
+        animator = GetComponent<Animator>();
+        animator.SetBool("isWalkingLateral", true);
+        gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+    }
+
+    bool turn = true;
+
     void Update(){
+
         if(tempo <= 5.0f){
+
+            if( turn ){
+
+                turn = false;
+                transform.localScale = new Vector3(0.002f, 0.002f, 1f);
+
+            }
+
             tempo += Time.deltaTime;
             transform.Translate( -(Time.deltaTime * speed), 0, 0);
             tempo2 = 0.0f;
         }
         else if(tempo2 <= 5.0f){
+
+            if( !turn ){
+
+                turn = true;
+                transform.localScale = new Vector3(-0.002f, 0.002f, 1f);
+
+            }
+
             tempo2 += Time.deltaTime;
             transform.Translate( Time.deltaTime * speed, 0, 0);
         }
